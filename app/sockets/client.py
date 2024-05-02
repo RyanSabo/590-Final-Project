e3448b3e2b1655c08c1dd814f81fa736df6cc5cc
Matthew Futch <Matthew.futch@unc.edu>
2024-05-02 19:45:34 -0400

    sockets

--- app/sockets/client.py
+++ app/sockets/client.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # Echo client program
 import socket
 
@@ -10,4 +11,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(bytes(msg, encoding="utf-8"))
     data = s.recv(1024)
+=======
+# Echo client program
+import socket
+
+HOST = '127.0.0.1'    # The remote host
+PORT = 50007              # The same port as used by the server
+f = open("msg.txt", "r")
+msg = f.readline()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(bytes(msg, encoding="utf-8"))
+    data = s.recv(1024)
+>>>>>>> Stashed changes
 print('Received', repr(data))
--- app/sockets/server.py
+++ app/sockets/server.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # Echo server program
 import socket
 
@@ -17,3 +18,24 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print(type(ct1), int(ct2))
         #
         conn.sendall(data)
+=======
+# Echo server program
+import socket
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        data = conn.recv(1024)
+        #
+        ct = data.decode().split(",")
+        
+        if len(ct) != None:
+            print(tuple(ct))
+        #
+        conn.sendall(data)
+>>>>>>> Stashed changes

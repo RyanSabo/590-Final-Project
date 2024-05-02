adc8c2553d9a43dc15c911dca8d1d0d12ec8282d
hashabadi <jkjacob9@gmail.com>
2024-05-02 02:37:19 -0400

    finished adding and testing crypto functions

--- app/cipher/elGamal.py
+++ app/cipher/elGamal.py
@@ -9,25 +9,84 @@ class ElGamal:
         self.p = 11774363152266578095067526258816830539166139106121849415818805169740043699372268857521995275805757855836228222867569659145695568012997698531776585688862219
         self.g = 2534890524550132494481097005281920111219579664822426722657432332577436305162772648334556738057313044668101379845453788943682939971615935369166151340598061
 
-        sk = random.randrange(1, self.p - 1)
-        self.key = eg.construct((self.p, self.g, pow(self.g, sk, self.p), sk))
+        self.x = random.randrange(1, self.p - 1)
+        self.y = pow(self.g, self.x, self.p)
 
+    def setPK(self, pk) : # get pk for group
+        self.pk = pk
+        return
+    
+    def setSK(self, sk) : # for testing decryption, not for actual use
+        self.x = sk
+        self.pk = pow(self.g, sk, self.p)
+        return
+
+    
     def encrypt(self, message:int) : # ElGamal encryption
         y = random.randrange(1, self.p - 1)
-        s = pow(self.key.y, y, self.p)
+        s = pow(self.y, y, self.p)
         c1 = pow(self.g, y, self.p)
         c2 = (message * int(s)) % self.p
         return (c1,c2)
 
     def decrypt(self, ct) : # ElGamal encryption
         c1, c2 = ct
-        s = pow(c1, int(self.key.x), self.p)
+        s = pow(c1, int(self.x), self.p)
         sInv = pow(s, self.p-2, self.p) # inverting by raising to p - 2
         m = (c2 * sInv) % self.p
         return m
 
-    def mult(self, element, factor) : # raise
-        return (factor * pow(int(self.g), int(self.key.x), int(self.p))) % self.p
+    def mult(self, factor, **kwargs) : # raise with given element, factor, and exponent
+        element = kwargs.get('element', self.g)
+        exp = kwargs.get('exp', self.x)
+        return (factor * pow(int(element), int(exp), int(self.p))) % self.p
+    
+    def vector(self, size) :
+        vector = list()
+        for i in range(size) :
+            vector.append(
+                self.encrypt(
+                    self.mult(1, element=self.pk, exp=i+1)))
+        return vector
+    
+    def shuffle(self, vector:list[int]) : # re-encrypt and shuffle given vector
+        for i in range(len(vector)) :
+            y = random.randrange(1, self.p - 1)
+            c1, c2 = vector[i]
+            c1 = self.mult(c1, exp=y)
+            c2 = self.mult(c2, element=self.pk, exp=y)
+            vector[i] = (c1,c2)
+        random.shuffle(vector)
+        return
+    
+    def divide(self, vector, vectorShuffle) :
+        quotient = list()
+        for i in range(len(vector)) :
+            c1Vector, c2Vector = vector[i]
+            c1Shuffle, c2Shuffle = vectorShuffle[i]
+            c1inverse = pow(c1Shuffle, self.p-2, self.p)
+            c2inverse = pow(c2Shuffle, self.p-2, self.p)
+            c1 = (c1Vector * c1inverse) % self.p
+            c2 = (c2Vector * c2inverse) % self.p
+            quotient.append((c1,c2))
+        return quotient
+    
+    def blind(self, ct) :
+        y = random.randrange(1, self.p-1)
+        c1, c2 = ct
+        c1 = pow(c1, y, self.p)
+        c2 = pow(c2, y, self.p)
+        return (c1,c2)
+    
+    def blindVector(self, vector) :
+        blinded = []
+        for ct in vector :
+            blinded.append(self.blind(ct))
+        return blinded
+    
+    def decShare(self, ct) : # returns share of inverse of s for decryption
+        c1 = pow(ct[0], self.x, self.p)
+        return pow(c1, self.p-2, self.p)
 
-    def multG(self, factor) : # if element is not specified, use generator g
-        return self.mult(self.g, factor)
+    
+    
--- app/cipher/main.py
+++ app/cipher/main.py
@@ -1,5 +1,6 @@
 from elGamal import ElGamal as eg
 from Cryptodome.Random.random import randrange
+from copy import deepcopy
 
 # encryption, decryption test
 message = 12345678910
@@ -11,16 +12,41 @@ print(message == result)
 
 # secret key share test
 shares = []
+ciphers = []
 groupSize = 5
 
 pkGen = 1
 for i in range(groupSize) :
     cipher = eg()
-    pkGen = cipher.multG(pkGen)
-    shares.append(int(cipher.key.x))
+    pkGen = cipher.mult(pkGen)
+    shares.append(int(cipher.x))
+    ciphers.append(cipher)
 
-sk = sum(shares)
+sk = sum(shares) % (cipher.p -1)
 pk = pow(cipher.g, sk, cipher.p)
 
+#print(pow(cipher.g, cipher.p, cipher.p))
+#print(pow(cipher.g, 1, cipher.p))
 print(f'Real key is {"not" if pk!=pkGen else ""}equal to generated key.\nReal:\n{pk}\nGenerated:\n{pkGen}')
 
+# re-encryption shuffle test
+master = eg()
+master.setSK(sk)
+
+for ciph in ciphers :
+    ciph.setPK(pk)
+vector = master.vector(groupSize)
+
+
+originalVector = deepcopy(vector)
+for ciph in ciphers :
+    ciph.shuffle(vector)
+
+quotient = master.divide(originalVector,vector)
+quotient = master.blindVector(quotient)
+for i in range(groupSize) :
+    vector[i] = master.decrypt(vector[i])
+    originalVector[i] = master.decrypt(originalVector[i])
+    quotient[i] = master.decrypt(quotient[i])
+
+print(quotient)

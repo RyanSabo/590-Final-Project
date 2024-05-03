8195e8edc753842415b112613cb3c48565c00245
Matthew Futch <Matthew.futch@unc.edu>
2024-05-02 21:11:58 -0400

    works

--- app/cipher/client.py
+++ app/cipher/client.py
@@ -1,22 +1,19 @@
 # Echo client program
 import socket
-from Cryptodome.PublicKey import ElGamal as eg
-
+from elGamal import ElGamal as eg
+from Cryptodome.Random import random
+import pickle
 HOST = '127.0.0.1'    # The remote host
 PORT = 50007              # The same port as used by the server
 f = open("msg.txt", "r")
 msg = f.readline()
 
-ciph = eg.eg()
-vector = ciph.vector()
-ctVector = []
-for msg in vector :
-    ctVector.append(ciph.encrypt(msg))
-    
-print(ctVector)
+ciph = eg()
+ciph.setPK(pow(ciph.g, random.randrange(1,ciph.p - 1), ciph.p))
+ctVector = ciph.vector(5)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(bytes(ctVector, encoding="utf-8"))
+    s.sendall(pickle.dumps(ctVector))
     data = s.recv(1024)
 print('Received', repr(data))
--- app/cipher/server.py
+++ app/cipher/server.py
@@ -1,5 +1,6 @@
 # Echo server program
 import socket
+import pickle 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -10,11 +11,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print('Connected by', addr)
         data = conn.recv(1024)
         #
-        ct = data.decode().split(",")
-        ct1, ct2 = ct[0], ct[1]
-        t = (ct1, ct2)
-
-        if len(ct) != None:
-            print(t)
+        ct = pickle.loads(data)
+        print(ct)
         #
-        conn.sendall(data)
